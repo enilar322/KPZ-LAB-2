2f26ffb07db72d6423e12a529dbe4b2e0353ed6a
Andrii Zotov <andialzo@gmail.com>
2025-11-02 16:36:11 +0200

    feat: change add signature to accept number[]

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,9 @@ import { config } from './config.js';
 
 // базові функції
 
-export function add(a: number, b: number): number {
-  return a + b;
+export function add(values: number[]): number {
+  return values.reduce((acc, x) => acc + x, 0);
 }
-
 export function capitalize(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1);
 }
